1dedec49007fb9b1c3620157bb691e3beaab1036
Sarthak Saraf <sarthak.saraf1@gmail.com>
2024-02-10 13:02:56 +0530

    fix/encoding-line-fixes

--- app.py
+++ app.py
@@ -11,34 +11,40 @@ def file_content():
     file_path = os.path.join(os.getcwd(), filename)
     if not os.path.exists(file_path):
         return "Error: File not found", 404
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+    encodings_to_try = ['utf-8', 'utf-16', 'utf-16-le', 'utf-16-be']
+    for encoding in encodings_to_try:
+        try:
+            with open(file_path, 'r', encoding=encoding) as file:
+                lines = file.readlines()
+            break 
+        except UnicodeDecodeError:
+            continue
     result = []
     if start and end:
         try:
             start = int(start)
             end = int(end)
-            if start <= 0 or end > len(lines):
+            if start < 0 or end > len(lines):
                 return "Error: Invalid line number", 400
-            result = lines[start-1:end+1]
+            result = lines[start:end+1]
         except ValueError:
             return "Error: start or end should be an integer", 400
     elif start:
         try:
             start = int(start)
             end = len(lines) - 1
-            if start <= 0 or end > len(lines):
+            if start < 0 or end > len(lines):
                 return "Error: Invalid line number", 400
-            result = lines[start-1:end+1]
+            result = lines[start:end+1]
         except ValueError:
             return "Error: lineNo should be an integer", 400
     elif end:
         try:
             start = 1
             end = int(end)
-            if start <= 0 or end > len(lines):
+            if start < 0 or end > len(lines):
                 return "Error: Invalid line number", 400
-            result =  lines[start-1:end+1]
+            result =  lines[start:end+1]
         except ValueError:
             return "Error: lineNo should be an integer", 400
     else:
