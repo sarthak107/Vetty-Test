241ebf769128753d5e3a39e377cf8948c1feb826
Sarthak Saraf <sarthak.saraf1@gmail.com>
2024-02-10 12:58:13 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def file_content():
+    filename = request.args.get('filename') if request.args.get('filename') else 'file1.txt'
+    start = request.args.get('start') 
+    end = request.args.get('end')
+    file_path = os.path.join(os.getcwd(), filename)
+    if not os.path.exists(file_path):
+        return "Error: File not found", 404
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    result = []
+    if start and end:
+        try:
+            start = int(start)
+            end = int(end)
+            if start <= 0 or end > len(lines):
+                return "Error: Invalid line number", 400
+            result = lines[start-1:end+1]
+        except ValueError:
+            return "Error: start or end should be an integer", 400
+    elif start:
+        try:
+            start = int(start)
+            end = len(lines) - 1
+            if start <= 0 or end > len(lines):
+                return "Error: Invalid line number", 400
+            result = lines[start-1:end+1]
+        except ValueError:
+            return "Error: lineNo should be an integer", 400
+    elif end:
+        try:
+            start = 1
+            end = int(end)
+            if start <= 0 or end > len(lines):
+                return "Error: Invalid line number", 400
+            result =  lines[start-1:end+1]
+        except ValueError:
+            return "Error: lineNo should be an integer", 400
+    else:
+        result = lines
+    response = ""
+    for line in result:
+        response += f"<p>{line}</p>"
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
